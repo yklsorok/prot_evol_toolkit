eea35fd787d0bd84a78b99e44c8de4b5a6d5a155
Anton Iershov <yklsorok@gmail.com>
2015-10-14 13:51:15 +0300

    script that compares two proteins by different protein scales added

---
+++ analyze_by_protscale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+''' This script analyzes the protein using different protein scales
+'''
+
+try:
+    pass
+except:
+    print "This script analyzes the protein using different protein scales."
